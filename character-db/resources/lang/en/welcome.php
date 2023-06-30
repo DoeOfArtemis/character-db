c0b3894c3f5184cfc9759d1c04fda7dc09594f3e
Emīlija Radzeviča <emilija.radzevica@gmail.com>
2023-06-30 10:33:31 +0300

    Dictionaries for localisation but no function

---
+++ character-db/resources/lang/en/welcome.php
@@ -0,0 +1,22 @@
+<?php
+return [
+    'Tēli<'=>'Characters',
+    'Profils<'=>'Profile',
+    'Izveidot tēlu'=>'Create character',
+    'Ieiet'=>'Log-in',
+    'Pierakstīties'=>'Sign-up',
+    'Tēlu katalogs<'=>'Character catalogue',
+    'Filtri'=>'Filters',
+    'Visi'=>'All',
+    'D&D'=>'D&D',
+    'Grāmatas'=>'Books',
+    'Autors'=>'Author',
+    'Po'=>'Po',
+    'Dzīvnieks'=>'Animal',
+    'Antropomorfisms'=>'Antropomorphism',
+    'Kung-Fu'=>'Kung-Fu',
+    'Elfs'=>'Elf',
+    'Izveidoja'=>'Created by',
+    'un'=>'and',
+]
+?>
